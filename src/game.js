219d2d0df519395dc69ebf6151f434dd2e20e71e
Stephen Niedzielski <stephen@niedzielski.com>
2016-09-13 23:27:17 -0600

    add jspm, phaser + boilerplate, live-server

---
+++ src/game.js
@@ -0,0 +1,10 @@
+import Phaser from './phaser.js' // eslint-disable-line no-unused-vars
+
+class Game {
+  /** @return {void} */
+  init() { // eslint-disable-line class-methods-use-this
+    console.log('hello world!') // eslint-disable-line no-console
+  }
+}
+
+export default new Game()
---
+++ src/phaser.js
@@ -0,0 +1,8 @@
+/* eslint-disable node/no-unpublished-require,node/no-missing-require */
+
+// https://github.com/photonstorm/phaser#browserify--cjs
+window.PIXI = require('phaser/build/custom/pixi')
+window.p2 = require('phaser/build/custom/p2')
+window.Phaser = require('phaser/build/custom/phaser-split')
+
+module.exports = require('phaser')
