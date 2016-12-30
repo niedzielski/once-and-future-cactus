8aa30c28f273ed3463ac7bfb0db364eb20e01e1f
Stephen Niedzielski <stephen@niedzielski.com>
2016-12-29 19:29:07 -0700

    new: support tileset collision polylines & many updates to tileset

--- src/Scene.js
+++ src/Scene.js
@@ -1,13 +1,23 @@
+// eslint-disable-line max-lines
 /// <reference path="Phaser.d.ts" />
 import Kbd from './Kbd'
 
+const TILEMAP_KEY = 'lvl'
+const TILEMAP_FILENAME = 'static/lvl.json'
+const TILESET16_KEY = 'tileset16'
+const TILESET16_FILENAME = 'static/tileset.png'
+const TILESET16_SIZE = 16
+const INCREMENT_X = 80
+const INCREMENT_Y = 120
+const GRAVITY_Y = 600
+
 /** @private {?Phaser.Tilemap} _map
-    @private {!Array.<Phaser.TilemapLayer>} _layers
+    @private {!Array.<Phaser.TilemapLayer>} _tileLayers
     @private {!Kbd} _kbd */
 export default class Scene {
   constructor() {
     this._map = null
-    this._layers = []
+    this._tileLayers = []
     this._kbd = new Kbd()
   }
 
@@ -24,77 +34,138 @@ export default class Scene {
   /** @param {!Phaser.Game} game
       @return {void} */
   preload(game) { // eslint-disable-line class-methods-use-this
-    game.stage.backgroundColor = '#f4f4ed'
-
-    const tileSize = 16
-    game.load.tilemap('lvl', 'static/lvl.json', null,
+    game.load.tilemap(TILEMAP_KEY, TILEMAP_FILENAME, null,
       Phaser.Tilemap.TILED_JSON)
-    game.load.spritesheet('tileset', 'static/tileset.png', tileSize, tileSize)
+    game.load.spritesheet(TILESET16_KEY, TILESET16_FILENAME, TILESET16_SIZE,
+      TILESET16_SIZE)
   }
 
   /** @param {!Phaser.Game} game
       @return {void} */
   create(game) {
-    this._map = game.add.tilemap('lvl')
-    this._map.addTilesetImage('tileset')
+    game.physics.startSystem(Phaser.Physics.P2JS)
+    game.physics.p2.gravity.y = GRAVITY_Y
 
-    for (const layer of this._map.layers) {
-      const made = this._map.createLayer(layer.name)
-      made.resizeWorld()
-      this._layers.push(made)
+    this._map = game.add.tilemap(TILEMAP_KEY)
+    this._map.addTilesetImage(TILESET16_KEY)
+
+    for (const obj of this._map.layers) {
+      const layer = this._map.createLayer(obj.name)
+      this._tileLayers.push(layer)
+      layer.resizeWorld()
     }
 
-    const chars = game.add.group()
-    // for (const key of Object.keys(this._map.objects)) {
-    //   console.log(`key=${key}`)
-    // }
-    const tileIndex = 128
-    this._map.createFromObjects('char', 'player', 'tileset', tileIndex, true,
-      false, chars)
+    for (const key of Object.keys(this._map.objects)) {
+      const layer = this._map.objects[key]
+      for (const obj of layer) {
+        this._map.createFromObjects(key, obj.name, TILESET16_KEY, obj.gid - 1)
+      }
+    }
 
-    chars.enableBody = true
-    this._player = chars.getChildAt(0)
+    this._loadCollisionBodies(game)
 
-    const center = .5
-    this._player.anchor.setTo(center, 0)
-    game.physics.arcade.enable(this._player)
-    this._player.body.collideWorldBounds = true
     game.camera.follow(this._player, Phaser.Camera.FOLLOW_PLATFORMER)
-
-    const maxVelocityPxPerSec = 50
-    this._player.body.maxVelocity.set(maxVelocityPxPerSec)
-    const dragPxPerSec = 300
-    this._player.body.drag.set(dragPxPerSec)
   }
 
   /** @param {!Phaser.Game} game
       @return {void} */
   update(game) {
-    const velocity = this._player.body.velocity, increment = 10
+    const velocity = this._player.body.velocity
+    velocity.x = 0
+
     if (this._kbd.up(game)) {
-      velocity.y -= increment
+      // consider just equals since that's snappier
+      // if (this._player.body.touching.down)
+      velocity.y -= INCREMENT_Y
     }
     if (this._kbd.right(game)) {
-      velocity.x += increment
+      this._translatePlayerX(game, false)
     }
     if (this._kbd.down(game)) {
-      velocity.y += increment
+      // if (!this._player.body.touching.down)
+      velocity.y += INCREMENT_Y
     }
     if (this._kbd.left(game)) {
-      velocity.x -= increment
+      this._translatePlayerX(game, true)
     }
   }
 
-  /** @param {!Phaser.Game} game
+  /** @param {!Phaser.Game} _game
       @return {void} */
-  render(game) {
-    game.debug.body(this._player)
+  render(_game) { // eslint-disable-line class-methods-use-this
   }
 
   /** @param {!Phaser.Game} game
       @return {void} */
   resize(game) {
-    this._layers.forEach(layer => layer.resize(this.width(), this.height()))
+    // todo: is this still needed?
+    this._tileLayers.forEach(layer => layer.resize(this.width(), this.height()))
     game.camera.follow(this._player, Phaser.Camera.FOLLOW_PLATFORMER)
+  }
+
+  /** @param {!Phaser.Game} game
+      @return {void} */
+  _loadCollisionBodies(game) {
+    for (const child of game.world.children) {
+      if (child.name !== 'player') continue // eslint-disable-line no-continue
+      this._player = child
+
+      // todo: how to go from name to gid?
+      const playerGid = 51
+      const collisions = this._findCollisionData(game, playerGid)
+      if (!collisions) continue // eslint-disable-line no-continue
+
+      game.physics.p2.enable(child)
+      child.body.clearShapes() // clear initial tile collision shape
+      child.body.debug = true
+
+      for (const collision of collisions) {
+        const polyline = this._polylineToArray(collision, collision.polyline)
+
+        // must be clockwise, must be symmetrical or never flip
+        child.body.loadPolygon(null, [{shape: polyline}])
+      }
+      // child.body.fixedRotation = true
+    }
+  }
+
+  /** @param {!Phaser.Point} origin
+    * @param {!Array.<Object>} polyline
+    * @return {!Array.<number>} */
+  // eslint-disable-next-line class-methods-use-this
+  _polylineToArray(origin, polyline) {
+    const arr = []
+    for (let p = 0; p < polyline.length; p++) {
+      arr.push(polyline[p].x + origin.x)
+      arr.push(polyline[p].y + origin.y)
+    }
+    return arr
+  }
+
+  /** @param {!Phaser.Game} game
+      @param {!number} gid
+      @return {?Object} */
+  _findCollisionData(game, gid) { // eslint-disable-line class-methods-use-this
+    const lvl = game.cache.getTilemapData(TILEMAP_KEY)
+    for (const tileset of lvl.data.tilesets) {
+      for (const tileGid of Object.keys(tileset.tiles)) {
+        if (tileGid === `${gid}`) {
+          const tile = tileset.tiles[gid]
+          return tile.objectgroup.objects
+        }
+      }
+    }
+    return null
+  }
+
+  /** @param {!Phaser.Game} game
+      @param {?boolean} left
+      @return {void} */
+  _translatePlayerX(game, left) {
+    const duration = 200
+    const sign = left ? -1 : 1
+    this._player.body.velocity.x = sign * INCREMENT_X
+    game.add.tween(this._player.scale)
+      .to({x: sign}, duration, Phaser.Easing.Bounce.Out, true)
   }
 }
